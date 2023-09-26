ae4e2e9a7d41ea9c7525130dbe947989018769ee
masayukiimai <ein4423@icloud.com>
2023-09-26 18:48:25 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "items#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
