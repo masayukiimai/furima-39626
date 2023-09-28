57839bd8c270b54dafefe8fb67bc600fa6c84ca2
masayukiimai <ein4423@icloud.com>
2023-09-28 17:25:35 +0900

    k

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
   get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
