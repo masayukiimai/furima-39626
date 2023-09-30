fac907157bf097a64119c353e207fa4aa9130b15
masayukiimai <ein4423@icloud.com>
2023-09-30 20:42:02 +0900

    k

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 
 
 class ApplicationController < ActionController::Base
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
 
 
 protected
@@ -14,7 +14,9 @@ def configure_permitted_parameters
 end
 
   private
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  #
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
-  devise_for :users
- 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "users#index"
-  resources :users, only: [:index]
+  root to: "users#index"
+  resources :users, only: [:index, :edit]
   # Defines the root path route ("/")
   # root "articles#index"
 end
 
-  
