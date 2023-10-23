49eb5dbd7073bd2620d4b36e7f18bd6d9b0c6317
masayukiimai <ein4423@icloud.com>
2023-10-23 21:27:19 +0900

    a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,23 @@
+class ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
 
-    class ItemsController < ApplicationController
-      
-      def new
-        @item = Item.new
-      end
-      
-      def create
-        @item = current_user.items.build(item_params)
-        if @item.save
-          redirect_to root_path
-        else
-          render :new
-        end
-      end
-      before_action :authenticate_user!, only: [:new, :create]
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  before_action :authenticate_user!, only: [:new, :create]
 
-      private
-      
-      def item_params
-        params.require(:item).permit(:name, :description, :price, :image, )
-      end
-    end      
-    
-  
+  private
 
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id,  :shipping_fee_id, :prefecture_id, :delivery_time_id,  :user)
+  end
+end
+
+   
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  # get 'items/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+   #get 'items/index'
+   #Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+   root to: "items#index"
+   resources :items, only: [:index,  :new, :create] 
   devise_for :users
 
 
-   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-   devise_scope :user do
-     get '/users/sign_out' => 'devise/sessions#destroy'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
    end
-  root to: "items#index"
-  resources :items, only: [:index,  :new, :create] 
-   resources :users, only: [:index,]
-   Defines the root path route ("/")
-   root "articles#index"
-end
+ 
+  # resources :users, only: [:index,]
+   #Defines the root path route ("/")
+  # root "articles#index"
 
+end
