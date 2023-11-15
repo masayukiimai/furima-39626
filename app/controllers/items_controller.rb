8f61c287ca6a01cf31889e315d8e4fe93752bdb0
masayukiimai <ein4423@icloud.com>
2023-11-15 17:28:43 +0900

    k

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
+
+
+
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+ 
+   before_action :set_item, only: [:show, :edit, :update]
+   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+   before_action :move_to_index, only: [:edit, :update]
   def new
     @item = Item.new
   end
@@ -21,10 +27,26 @@ class ItemsController < ApplicationController
     end
     
   
+def show
+end
+
+   def edit
+   end
+
+   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+     else
+       render :edit
+     end
+   end
   
   
   
   private
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :price, :category_id, :condition_id,  :shipping_fee_id, :region_id, :shipping_time_id).merge(user: current_user)
@@ -32,6 +54,17 @@ class ItemsController < ApplicationController
 end
 
 
+   def move_to_index
+   redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
+   end
+
+
+
+
+  
+
+
+   
 
 
 
