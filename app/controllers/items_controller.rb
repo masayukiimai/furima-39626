ce103e81f0750a8da2e9e54ed405bd75303ca2ee
masayukiimai <ein4423@icloud.com>
2023-11-18 21:52:17 +0900

    削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,13 +52,13 @@ end
       redirect_to item_path(@item)
     end
   end
-
-  
   
   private
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.find_by(id: params[:id])
+    redirect_to root_path, alert: "Item not found" if @item.nil?
   end
+  
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :price, :category_id, :condition_id,  :shipping_fee_id, :region_id, :shipping_time_id).merge(user: current_user)
@@ -83,7 +83,15 @@ end
 
 
 
-  
+  def destroy
+        @prototype = Prototype.find(params[:id])
+        if @prototype.destroy
+          redirect_to root_path
+        else
+          redirect_to root_path
+        end
+      end
+    
 
 
    
