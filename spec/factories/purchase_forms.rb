7309a9abcac884073c5a1317368606c2a470058f
masayukiimai <ein4423@icloud.com>
2023-12-29 04:47:56 +0900

    k

--- spec/factories/purchase_forms.rb
+++ spec/factories/purchase_forms.rb
@@ -5,6 +5,7 @@
         region_id { 1 }
         city { '東京都' }
         address { '渋谷1-1-1' }
+        building { '渋谷ビル101' }
         phone_number { '09012345678' }
         token { 'tok_abcdefghijk00000000000000000' }
   
