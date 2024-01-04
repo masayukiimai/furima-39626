dd094e2b6f766e5260eb39efd770962a295df222
masayukiimai <ein4423@icloud.com>
2024-01-04 20:22:21 +0900

    region

--- db/migrate/20231015062706_create_items.rb
+++ db/migrate/20231015062706_create_items.rb
@@ -7,6 +7,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.integer :category_id, null: false
       t.integer :condition_id, null: false
       t.integer :shipping_fee_id, null: false
+      t.integer :region_id, null: false
       t.integer :prefecture_id, null: false
       t.integer :delivery_time_id, null: false
       t.references :user, foreign_key: true  
