97ece74fd707787847c913724be732d59577af04
masayukiimai <ein4423@icloud.com>
2024-01-05 15:16:57 +0900

    payjpの変更

--- app/javascript/payment.js
+++ app/javascript/payment.js
@@ -1,41 +1,32 @@
 const pay = () => {
   const publicKey = gon.public_key
-  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
-  const elements = payjp.elements();
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
+  const payjp = Payjp(publicKey) //PAY.JPテスト公開鍵
+  const elements = payjp.elements()
+  const numberElement = elements.create('cardNumber')
+  const expiryElement = elements.create('cardExpiry')
+  const cvcElement = elements.create('cardCvc')
 
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
-
-  const form = document.getElementById('charge-form');
-  let tokenCreated = false; // トークンが生成されたかどうかを追跡するフラグ
-
+  numberElement.mount('#number-form')
+  expiryElement.mount('#expiry-form')
+  cvcElement.mount('#cvc-form')
+  const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
-    e.preventDefault(); // デフォルトのフォーム送信を防止
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id
+        const renderDom = document.getElementById("charge-form")
+        const tokenObj = `<input value=${token} name='token' type="hidden">`
+        renderDom.insertAdjacentHTML("beforeend", tokenObj)
+      }
+      numberElement.clear()
+      expiryElement.clear()
+      cvcElement.clear()
+      document.getElementById("charge-form").submit()
+    })
+    e.preventDefault()
+  })
+}
 
-    if (!tokenCreated) {
-      payjp.createToken(numberElement).then(function (response) {
-        if (response.error) {
-          // トークン生成エラーのハンドリング
-        } else {
-          const token = response.id;
-          const renderDom = document.getElementById("charge-form");
-          const tokenObj = `<input value=${token} name='token' type="hidden">`;
-          renderDom.insertAdjacentHTML("beforeend", tokenObj);
-
-          tokenCreated = true; // トークンが生成されたことを記録
-          form.submit(); // トークンが生成された後にフォームを送信
-        }
-        numberElement.clear();
-        expiryElement.clear();
-        cvcElement.clear();
-      });
-    }
-  });
-};
-
-window.addEventListener("turbo:load", pay);
-window.addEventListener("turbo:render", pay);
+window.addEventListener("turbo:load", pay)
+window.addEventListener("turbo:render", pay)
